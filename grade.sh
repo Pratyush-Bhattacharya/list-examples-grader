49043558794d4e48d64118d1c783d94ea12ff1c0
Pratyush-Bhattacharya <156235712+Pratyush-Bhattacharya@users.noreply.github.com>
2024-02-14 11:50:44 -0800

    failed

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;/../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -8,6 +8,32 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ $(find student-submission/ -name "ListExamples.java") = "student-submission/ListExamples.java" ]
+then
+    echo 'found'
+    cp -r student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+    cd grading-area
+    javac -cp $CPATH *.java
+    echo $?
+    if [ $? == 0 ]
+    then
+        java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+        if [ $? == 0 ]
+        then
+            echo "Sucess!"
+        else
+            echo "Failed test"
+        fi
+    else
+        echo "failed compilation error"
+    fi
+else 
+    echo 'not found'
+    echo 'instead found'
+    found= find student-submission/ -name "ListExamples.java"
+    echo $found
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
